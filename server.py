843eb28f1433e43474e19e91c16ec73bca25f7a8
cindywindy <salc3657@gmail.com>
2020-02-23 03:57:27 -0800

    .

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from forms import SignUpForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'upliftedu'
 
@@ -9,6 +10,11 @@ def home():
 @app.route('/profile/<int:profile_id>')
 def profile(profile_id):
     return 'Profile number: ' + str(profile_id)
+
+@approute('/donorSignUp'):
+def donorSignUp():
+    form = SignUpForm()
+    return render_template('DonorSignUp.html, form=form')
 
 if __name__ == '__main__':
     app.run()
